b47433b4746c3e1ef0cc08590335b6c390c4816d
Naoya Yoshimura <yoshimura708x@gmail.com>
2025-10-25 21:36:25 +0900

    fix: change default port for e2e test

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,13 +3,13 @@ import { defineConfig, devices } from '@playwright/test'
 export default defineConfig({
   testDir: './tests/e2e',
   webServer: {
-    command: 'npm run dev', // Start the development server
-    port: 3000,
+    command: 'npm run dev-e2e-test', // Start the development server
+    port: 3030,
     reuseExistingServer: false, // Do not reuse existing server, let Playwright start it
     timeout: 120_000, // Longer timeout for slower environments
   },
   use: {
-    baseURL: 'http://localhost:3000',
+    baseURL: 'http://localhost:3030',
   },
   projects: [{ name: 'chromium', use: { ...devices['Desktop Chrome'] } }],
   // Optional CI settings

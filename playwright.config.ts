1d51627c1ebcc0aabec8de43ea18da679931663c
Naoya Yoshimura <yoshimura708x@gmail.com>
2025-10-26 10:16:20 +0900

    fix: e2e test

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,10 +3,11 @@ import { defineConfig, devices } from '@playwright/test'
 export default defineConfig({
   testDir: './tests/e2e',
   webServer: {
-    command: 'npm run dev-e2e-test', // Start the development server
+    command: 'npm run dev:e2e', // Start the development server
     port: 3030,
     reuseExistingServer: false, // Do not reuse existing server, let Playwright start it
     timeout: 120_000, // Longer timeout for slower environments
+    url: 'http://localhost:3030',
   },
   use: {
     baseURL: 'http://localhost:3030',

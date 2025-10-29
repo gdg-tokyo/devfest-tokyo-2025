2e856deb69f01a1d28c6330e38ea9397ce163a36
Naoya Yoshimura <yoshimura708x@gmail.com>
2025-10-30 00:36:37 +0900

    fix: Adjust event card height

--- src/features/landing-page/components/EventOverview.tsx
+++ src/features/landing-page/components/EventOverview.tsx
@@ -17,7 +17,7 @@ const EventOverview = () => {
       data-testid="event-overview-section"
     >
       <div>
-        <div className="flex flex-wrap items-stretch">
+        <div className="flex flex-wrap items-start md:items-stretch">
           <div className="w-full md:w-4/12 px-4 text-center flex-grow">
             <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg border-2 border-gray-800 h-full">
               <div className="px-4 py-5 flex-auto">

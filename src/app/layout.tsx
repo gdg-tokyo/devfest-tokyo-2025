8f3335278985b54754e21e76a2af9b43e5d0083e
Naoya Yoshimura <yoshimura708x@gmail.com>
2025-10-30 01:57:13 +0900

    feat: add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,9 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'DevFest Tokyo 2025',
   description: 'DevFest Tokyo 2025 - Find your new “eyes”',
+  icons: {
+    icon: '/images/favicon.ico',
+  },
 }
 
 import Header from '@/components/Header'

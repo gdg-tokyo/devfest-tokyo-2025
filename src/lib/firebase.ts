1726488a5123fad71c1b9684129ba26cd62ae88d
Yoshimura Naoya <yoshimura708x@gmail.com>
2025-11-02 18:50:46 +0900

    fix: Migrate Google Analytics to @next/third-parties (#30)

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,15 +1,12 @@
 import { initializeApp, getApps, getApp, FirebaseOptions } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 
 function initializeFirebase(firebaseConfig: FirebaseOptions) {
   if (!getApps().length) {
     const app = initializeApp(firebaseConfig);
-    const analytics = getAnalytics(app);
-    return { app, analytics };
+    return { app };
   } else {
     const app = getApp();
-    const analytics = getAnalytics(app);
-    return { app, analytics };
+    return { app };
   }
 }
 

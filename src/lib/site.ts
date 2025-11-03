b2273f3c3e319b489ebc92f7c0786e515a62da99
Naoya Yoshimura <yoshimura708x@gmail.com>
2025-11-03 09:29:00 +0900

    feat: Update Twitter account and fix duplicated page titles for timetable/ and talks/

--- src/lib/site.ts
+++ src/lib/site.ts
@@ -1,7 +1,13 @@
 export const SITE = {
-  name: 'Google Developer Group - DevFest Tokyo 2025',
-  defaultTitle: 'GDG DevFest Tokyo 2025',
+  name: "Google Developer Group - DevFest Tokyo 2025",
+  defaultTitle: "GDG DevFest Tokyo 2025",
   defaultDescription:
-    'Google Developer Group - DevFest Tokyo 2025 の公式サイトです。セッションやその他イベントに関する情報をお届けします。',
-  url: process.env.NEXT_PUBLIC_SITE_URL ?? 'http://localhost:3000',
+    "Google Developer Group - DevFest Tokyo 2025 の公式サイトです。セッションやその他イベントに関する情報をお届けします。",
+  url: process.env.NEXT_PUBLIC_SITE_URL ?? "http://localhost:3000",
+  defaultOgImage: "/images/pr/gdg-devfest-2025-main-banner-compressed.jpg", // Recommended 1200x630
+  locale: "ja_JP",
+  twitter: {
+    site: "@gdgtokyo",    // if available
+    creator: "@gdgtokyo", // if available
+  },
 };
